ba904eda53833fc348dbb90d70efbf81aa9bd60e
Ihssaneabousshal <ihssaneaboussahl@gmail.com>
2023-08-07 12:29:38 +0100

    2

--- server.js
+++ server.js
@@ -12,32 +12,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27018/hotel_reservation', {
+/* mongoose.connect('mongodb://localhost:27017/hotel_reservation', {
   useNewUrlParser: true, 
   useUnifiedTopology: true, 
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error);
-});
+}); */
+setTimeout(async function() {
+  try {
+    await mongoose.connect("mongodb://db:27017/hotel_reservation", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected ;)');
+
+  } catch (error) {
+    console.log('Error connecting - retrying in 30 seconds');
+  }
+}, 30000);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/api', reservationRoutes);
 app.use('/api/myspace', hotelRoutes);
 app.use('/api/myspace', ExperienceRoutes);
-
-// Create a default admin user if not exists
-User.findOne({ isAdmin: true }).then(user => {
-  if (!user) {
-    const adminUser = new User({
-      username: 'admin',
-      password: 'adminpassword', // You should hash this password
-      isAdmin: true
-    });
-    adminUser.save();
-  }
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
